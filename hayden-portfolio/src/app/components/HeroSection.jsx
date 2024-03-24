8c570cb970e0ce97975738a75705458db3a752c1
Hayden Kiltoff <hayden060@gmail.com>
2024-03-24 13:56:33 -0700

    heading and framing started

---
+++ hayden-portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section>
+            <div className='grid grid-cols-1 lg:grid-cols-12'>
+                <div className='col-span-7 place-self-center'>
+            <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello,  im hayden</h1>
+            <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias ex dolor praesentium! Recusandae mollitia id quibusdam excepturi perferendis sapiente quaerat ullam? Nisi doloremque iste assumenda, obcaecati totam magnam iusto quidem?
+            </p>
+            </div>
+            <div className='col-span-5'>
+
+            </div>
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
